31964c248ee60b7dc72c82b0ba064435253e84cf
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2020-10-29 16:07:51 -0400

    with server

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const server = require("./server.js");
+// const express = require('express')
+// require("dotenv").config();
+const port = process.env.PORT || 3333;
 
-server.listen(4000, () => {
-  console.log("\n*** Server Running on http://localhost:4000 ***\n");
+const server = require("./server.js");
+const secret = process.env.SECRET || "foo";
+console.log(port, secret);
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
